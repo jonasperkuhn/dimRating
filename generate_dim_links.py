16b22e77b1f96257b9750ffc1807b5b5f383874b
jonasperkuhn <joperkuhn@gmx.de>
2021-05-31 12:30:22 +0200

    changed to 4 digit image codes and added comments

--- generate_dim_links.py
+++ generate_dim_links.py
@@ -4,14 +4,19 @@ data_path = 'C:/Users/joper/PycharmProjects/dimRating/data/'  # set path to data
 n_dim = 49
 n_anchors = 7  # anchors plus one to get bins
 # load data
-y = np.loadtxt(data_path + 'spose_embedding_49d_sorted.txt')  # load y
+y = np.loadtxt(data_path + 'spose_embedding_49d_sorted.txt')  # load y; path to folder resources
 dim_imgs = dict()
 for i_dim in range(n_dim):
     dim_scores = y[:, i_dim]
     range_scores = max(dim_scores) - min(dim_scores)
     dim_dict = dict()
     for i_anchor in range(n_anchors):
+        # determine lower-bound score of anchor (e.g., 0.578 corresponds to 20%)
         dim_score_lowest = min(dim_scores) + range_scores * (i_anchor/n_anchors)
+        # determine upper-bound score of anchor (e.g., 0.765 corresponds to 40%)
         dim_score_highest = min(dim_scores) + range_scores * ((i_anchor + 1)/n_anchors)
-        dim_dict[i_anchor] = np.where((dim_scores > dim_score_lowest) & (dim_scores <= dim_score_highest))[0]
+        # select indices of images between lowest (excl.) and highest (incl.) scoring image in anchor range
+        dim_dict[i_anchor] = list(np.where((dim_scores > dim_score_lowest) & (dim_scores <= dim_score_highest))[0])
+        for ind, img_code in enumerate(dim_dict[i_anchor]):  # format to four digits with leading 0s (like on website)
+            dim_dict[i_anchor][ind] = str(img_code).zfill(4)
     dim_imgs[i_dim] = dim_dict
---
+++ generate_trial_csv.py
@@ -0,0 +1,3 @@
+n_dims = 49
+n_vp_per_dim = 40
+
