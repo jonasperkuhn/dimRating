84edeeb28ec5d8b2d3756e704eaeee2b9347f0b7
jonasperkuhn <joperkuhn@gmx.de>
2021-05-31 18:00:44 +0200

    minor edits

--- generate_dim_links.py
+++ generate_dim_links.py
@@ -16,5 +16,7 @@ for i_anchor in range(n_anchors):
     dim_score_highest = min(dim_scores) + range_scores * ((i_anchor + 1)/n_anchors)
     # select indices of images between lowest (excl.) and highest (incl.) scoring image in anchor range
     dim_img_codes[i_anchor] = list(np.where((dim_scores > dim_score_lowest) & (dim_scores <= dim_score_highest))[0])
+    # todo: sort from closest to anchor to furthest away; not yet sure how
+    # todo: remove previously rated 20 images (will be tested)
     for ind, img_code in enumerate(dim_img_codes[i_anchor]):  # format to four digits with leading 0s (like on website)
         dim_img_codes[i_anchor][ind] = str(img_code).zfill(4)
