8b886d6724fb36acb2e0d5bdb52e1a9f3ae305bc
jonasperkuhn <joperkuhn@gmx.de>
2021-06-01 13:59:25 +0200

    creates block trial files per dim and saves them as .csv's

--- generate_exp_trial_csv.py
+++ generate_exp_trial_csv.py
@@ -1,30 +1,49 @@
+import random
 import numpy as np
-data_path = 'C:/Users/joper/PycharmProjects/dimRating/data/'  # set path to data folder
+path = 'C:/Users/joper/PycharmProjects/dimRating/'  # set path to data folder
+n_blocks = 2  # set to a number that 96, 20, and 116 can be divided by -> 2 or 4
 # load data
-y = np.loadtxt(data_path + 'spose_embedding_49d_sorted.txt')  # load y; path to folder resources
+y = np.loadtxt(path + 'data/spose_embedding_49d_sorted.txt')  # load y; path to folder resources
     # load indices of previously rated 20 images
-test_ref_imgs = list(np.arange(20)*3)
-for ind, img_code in enumerate(test_ref_imgs):  # format to four digits with leading 0s (like on website)
-    test_ref_imgs[ind] = str(img_code).zfill(4)
+test_ref_imgs_ind = list(np.arange(20)*3)
+test_ref_imgs = []
+for ind, img_code in enumerate(test_ref_imgs_ind):  # format to four digits with leading 0s (like on website)
+    test_ref_imgs.append(str(img_code).zfill(4))
     # load/generate list of 48 not-ref spose image id's
-test_non_ref_imgs = list(np.arange(20)*5)
+test_non_ref_imgs = list(np.arange(48)*5)
 for ind, img_code in enumerate(test_non_ref_imgs):  # format to four digits with leading 0s (like on website)
     test_non_ref_imgs[ind] = str(img_code).zfill(4)
     # load list of 48 new image id's
-test_new_imgs = list(np.arange(20) * 7)
+test_new_imgs = list(np.arange(48) * 7)
 for ind, img_code in enumerate(test_new_imgs):  # format to four digits with leading 0s (like on website)
     test_new_imgs[ind] = str(img_code).zfill(4)
-# initialize experimental trial matrix: mat = np.zeros((116,3))
-
-# loop over all dims
-    # for dim_id in range(np.size(y,1)):
 # combine 48 and 48 to all_no-feedback_trials and randomize
-# add 20 trials
-# add true_dim_score for feedback img
-# set feedback to 1 for feedback img
-# randomize 20 imgs
-# create 2 or 4 block files
-# 48 + 10 images per file
-# in psychopy: if true_dim_score, then run feedback routine, else continue
-
-# save as csv
+trials_fb = test_ref_imgs  # don't random shuffle to keep alignment to indices
+trials_nofb = test_non_ref_imgs + test_new_imgs
+random.shuffle(trials_nofb)
+# calc trial numbers
+n_trials = len(trials_fb + trials_nofb)
+n_trials_per_block = int(n_trials/n_blocks)
+n_trials_fb_per_block = int(len(trials_fb) / n_blocks)
+n_trials_nofb_per_block = int(len(trials_nofb) / n_blocks)
+# loop over dims (for getting true_dim_scores)
+for dim_id in range(np.size(y,1)):
+    # create trial matrix blockwise
+    for block in range(n_blocks):
+        # initialize experimental trial matrix
+        trial_mat = np.zeros((n_trials_per_block, 3))
+        # add 20/n_blocks trials
+        trials_fb_block = range(block*n_trials_fb_per_block, (block+1)*n_trials_fb_per_block)
+        trial_mat[0:n_trials_fb_per_block, 0] = [trials_fb[i] for i in trials_fb_block]
+        # add true_dim_score for feedback img
+        trial_mat[0:n_trials_fb_per_block, 1] = [y[test_ref_imgs_ind[i], dim_id] for i in trials_fb_block]
+        # set feedback to 1 for feedback img
+        trial_mat[0:n_trials_fb_per_block, 2] = 1
+        # add 96/n_blocks trials
+        trials_nofb_block = range(block*n_trials_nofb_per_block, (block+1)*n_trials_nofb_per_block)
+        trial_mat[n_trials_fb_per_block:n_trials_per_block, 0] = [trials_nofb[i] for i in trials_nofb_block]
+        # save as csv
+        header = 'img_code,true_dim_score,feedback'
+        fname = path + 'exp_trial_csvs/exptrials_dim' + str(dim_id) + '_block' + str(block) + '.csv'
+        np.savetxt(fname, trial_mat, delimiter=",", header=header)
+        # in psychopy: if true_dim_score, then run feedback routine, else continue
