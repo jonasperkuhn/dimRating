80d5a274163a146b816f0ce82fa57d2d177d7038
jonasperkuhn <joperkuhn@gmx.de>
2021-06-03 15:43:20 +0200

    changed to percentile method, above zero cutoff

--- generate_dim_links.py
+++ generate_dim_links.py
@@ -2,21 +2,31 @@ import numpy as np
 # set params
 data_path = 'C:/Users/joper/PycharmProjects/dimRating/data/'  # set path to resources folder
 dim_id = 0  # set dim id
-n_anchors = 7  # count of anchors
+n_anchors = 6  # count of anchors
+zero_cutoff = 0.3
 # load data
 y = np.loadtxt(data_path + 'spose_embedding_49d_sorted.txt')  # load y; path to folder resources
-# get image codes for dim visualization
+# select scores of relevant dimension
 dim_scores = y[:, dim_id]
-range_scores = max(dim_scores) - min(dim_scores)
-dim_img_codes = dict()
+# initialize img_code dict
+img_codes = dict()
+# enter image codes of images below cut-off to 'notatall'
+img_ind_zero = list(np.where(dim_scores <= zero_cutoff)[0])
+# format to four digits with leading 0s (like on website), and save in dict
+img_codes['notatall'] = [str(img_code).zfill(4) for img_code in img_ind_zero]
+# get indices
+img_ind_nonzero = list(np.where(dim_scores > zero_cutoff)[0])
+# extract image codes for each anchor range, and sort from highest to lowest
 for i_anchor in range(n_anchors):
     # determine lower-bound score of anchor (e.g., 0.578 corresponds to 20%)
-    dim_score_lowest = min(dim_scores) + range_scores * (i_anchor/n_anchors)
+    score_lowest = np.percentile(dim_scores[img_ind_nonzero], i_anchor / n_anchors * 100)
     # determine upper-bound score of anchor (e.g., 0.765 corresponds to 40%)
-    dim_score_highest = min(dim_scores) + range_scores * ((i_anchor + 1)/n_anchors)
+    score_highest = np.percentile(dim_scores[img_ind_nonzero], (i_anchor + 1) / n_anchors * 100)
     # select indices of images between lowest (excl.) and highest (incl.) scoring image in anchor range
-    dim_img_codes[i_anchor] = list(np.where((dim_scores > dim_score_lowest) & (dim_scores <= dim_score_highest))[0])
-    # todo: sort from closest to anchor to furthest away; not yet sure how
+    img_codes_unsorted = list(np.where((dim_scores > score_lowest) & (dim_scores <= score_highest))[0])
+    # get indices of anchor images, sorted from highest to lowest
+    sorted_indices = list([np.argsort(dim_scores[img_codes_unsorted])][0])
+    img_codes_sorted = [img_codes_unsorted[img_code] for img_code in sorted_indices]
     # todo: remove previously rated 20 images (will be tested)
-    for ind, img_code in enumerate(dim_img_codes[i_anchor]):  # format to four digits with leading 0s (like on website)
-        dim_img_codes[i_anchor][ind] = str(img_code).zfill(4)
+    # format to four digits with leading 0s (like on website), and save in dict
+    img_codes[i_anchor] = [str(img_code).zfill(4) for img_code in img_codes_sorted]
