2978249a2e3b3827bafc1363b416ba9dcfbc99f3
jonasperkuhn <joperkuhn@gmx.de>
2021-06-18 12:26:04 +0200

    removed calc, now true percentile scores as input

--- feedback_code.py
+++ feedback_code.py
@@ -1,34 +1,24 @@
+### feedback code
 ## end feedback routine, if feedback variable in conditions file = 0
 if feedback == 0:
     continueRoutine = False # end routine
-
 ## reset scale position
 dim_rater_fb.pos = (pos_scale_x, pos_scale_y)
 
+n_anchors = 7
 ## calculate performance and feedback
-scale_steps = n_anchors + 1  # absolute steps are 7 (-1 to 5)
-scale_pos_steps = n_anchors - 1  # positive scale goes from 0 to 5
-# get true dim scores
-if dim_score_true <= zero_cutoff:  # if true dim is 'not at all' (below cutoff)
-    dim_score_true_ptile = -1
-    pos_dim_true = pos_notatall_y - 0.02
-else:
-    i_score_true = np.abs(np.array(dim_scores_nonzero) - dim_score_true).argmin()  # find index of true dim score img
-    dim_score_true_ptile = ptiles[i_score_true]  # select corresponding percentile
-    pos_dim_true = (pos_anchors_y[0] + size_scale_y * dim_score_true_ptile / 100)
-# get rated dim scores
-if dim_rater_trial.getRating() is None:  # if rated 'not at all'
-    score_rated_ptile = -1
-    pos_dim_rated = pos_notatall_y - 0.02
-else:
-    score_rated = dim_rater_trial.getRating()
-    score_rated_ptile = score_rated / scale_pos_steps * 100  # scale rated score to percentile range
-    i_score_rated = np.abs(np.array(ptiles) - score_rated_ptile).argmin()  # find index of closest
-    score_rated_spose = dim_scores_nonzero[i_score_rated]  # select corresponding percentile
-    pos_dim_rated = (pos_anchors_y[0] + size_scale_y * score_rated_ptile / 100)
+# get rated dim scores and convert to percentile scale
+score_rated = dim_rater_trial.getRating()
+score_rated_ptile = (score_rated-1) / (n_anchors-1) * 100  # scale rated score to percentile range
+
+# get scale position of rated and true dim scores
+pos_dim_rated = (pos_anchors_y[0] + size_scale_y * score_rated_ptile / 100)
+pos_dim_true = (pos_anchors_y[0] + size_scale_y * dim_score_true_ptile / 100)
 
 # determine difference between rated and true dim score in percent
 diff_percent_ptile = round(abs(score_rated_ptile - dim_score_true_ptile))
+# collect deviations for all trials, to calc block performance
+deviation_list.append(diff_percent_ptile)
 
 # determine color of deviation rectangle
 if diff_percent_ptile < 1:
@@ -42,11 +32,8 @@ else:
     deviation_color = 'red'
 
 # set percent deviation as string for display
-if dim_score_true_ptile == -1 and diff_percent_ptile > 0:
-    deviation_message.text = ('Consider the\n"not at all"\nanchor')
-elif score_rated_ptile == -1 and diff_percent_ptile > 0:
-    deviation_message.text = ('Consider\nthe higher\nanchors')
-else:
-    deviation_message.text = (str(diff_percent_ptile) + "% off")
+deviation_message.text = (str(diff_percent_ptile) + "% off")
 # determine the positions to highlight on slider scale
 pos_diff = abs(pos_dim_rated - pos_dim_true)
+# only for debugging:
+print_scores_txt = (str(dim_score_true) + 'and' + str(dim_id) + 'and' + str(score_rated_ptile))
