2d62db1d719d375b7a56a42942060051e05b0c98
jonasperkuhn <joperkuhn@gmx.de>
2021-06-18 13:18:32 +0200

    unadjusted script! new version in combined script

--- generate_exp_trial_csv.py
+++ generate_exp_trial_csv.py
@@ -9,15 +9,15 @@ random.seed(808)
 header = 'img_code,dim_score_true,feedback'
 
 # load data
-y = np.loadtxt(path + 'data/spose_embedding_49d_sorted.txt')  # load y; path to folder resources
+spose = np.loadtxt(path + 'data/spose_embedding_49d_sorted.txt')  # load y; path to folder resources
     # load indices of previously rated 20 images
-test_ref_imgs = list(np.loadtxt(path + 'data/ref_imgs_20.txt'))
+stim_imgs_20 = list(np.loadtxt(path + 'data/ref_imgs_20.txt'))
     # load/generate list of 48 not-ref spose image id's
 test_non_ref_imgs = list(np.arange(48)*5)  # todo: remove * 5 for actual construction
     # load list of 48 new image id's
 test_new_imgs = list(np.arange(48) * 7)  # todo: remove * 7
 # combine 48 and 48 to all_no-feedback_trials and randomize
-trials_fb = test_ref_imgs  # don't random shuffle to keep alignment to indices
+trials_fb = stim_imgs_20  # don't random shuffle to keep alignment to indices
 trials_nofb = test_non_ref_imgs + test_new_imgs
 random.shuffle(trials_nofb)
 # calc trial numbers
@@ -26,9 +26,9 @@ n_trials_per_block = int(n_trials/n_blocks)
 n_trials_fb_per_block = int(len(trials_fb) / n_blocks)
 n_trials_nofb_per_block = int(len(trials_nofb) / n_blocks)
 # loop over dims (for getting true_dim_scores)
-for dim_id in range(np.size(y,1)):
+for dim_id in range(np.size(spose, 1)):
     # select dim data and compute range of values larger than zero-cutoff
-    dim_scores = y[:, dim_id]
+    dim_scores = spose[:, dim_id]
     range_scores_nonzero = max(dim_scores) - min(dim_scores[np.where(dim_scores > 0.3)])
     # create trial matrix blockwise
     for block in range(n_blocks):
@@ -38,7 +38,7 @@ for dim_id in range(np.size(y,1)):
         trials_fb_block = range(block*n_trials_fb_per_block, (block+1)*n_trials_fb_per_block)
         trial_mat[0:n_trials_fb_per_block, 0] = [trials_fb[i] for i in trials_fb_block]
         # add true_dim_score for feedback img
-        trial_mat[0:n_trials_fb_per_block, 1] = [y[int(test_ref_imgs[i]), dim_id] for i in trials_fb_block]
+        trial_mat[0:n_trials_fb_per_block, 1] = [spose[int(stim_imgs_20[i]), dim_id] for i in trials_fb_block]
         # set feedback to 1 for feedback img
         trial_mat[0:n_trials_fb_per_block, 2] = 1
         # add 96/n_blocks feedback trials
@@ -47,6 +47,4 @@ for dim_id in range(np.size(y,1)):
         # save as csv
         fname = path + 'trial_csvs/dim' + str(dim_id) + '_exptrials_block' + str(block) + '.csv'
         np.savetxt(fname, trial_mat, delimiter=",", header=header, comments='')
-        # in psychopy: if true_dim_score, then run feedback routine, else continue
-        # in psychopy: compute true_dim_score_percent
-        # in psychopy: format img codes according to website code (4 digits, starting from 0001)
+
