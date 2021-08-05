9413fbddafba3430871ee53f3c6fe8c2ee129024
jonasperkuhn <joperkuhn@gmx.de>
2021-08-05 15:02:04 +0200

    changed function inputs

--- generate_dimrating_input_data.py
+++ generate_dimrating_input_data.py
@@ -3,7 +3,7 @@ import pickle
 import random
 from shutil import copyfile, copytree
 
-def get_data(path_input = path_input):
+def get_data(path_input):
     spose = np.loadtxt(path_input + 'spose_embedding_49d_sorted.txt')  # load true dim scores
     stim_imgs_20 = np.loadtxt(path_input + 'ref_imgs_20.txt')  # image codes of reused images from old experiment
     stim_imgs_20 = [int(i) for i in list(stim_imgs_20)]  # convert to list of integers
@@ -13,7 +13,7 @@ def get_data(path_input = path_input):
     stim_imgs_48_new = list(np.arange(start=3000, stop=3048))
     return spose, stim_imgs_20, stim_imgs_48_nonref, stim_imgs_48_new
 
-def get_dim_paths(dim_id = dim_id, path_exps_final = path_exps_final):
+def get_dim_paths(dim_id, path_exps_final):
     # copy template experiment folder n_dims times, to create separate experiments for each spose dimension
         # first create template experiment folder manually, with resources subfolders for condition files, screenshots,
         # and test images, as well as the mouse-cursor-img.png
@@ -23,7 +23,7 @@ def get_dim_paths(dim_id = dim_id, path_exps_final = path_exps_final):
     path_output = dim_exp_folder_path + 'resources/'
     return path_output
 
-def get_ptiles(dim_scores = dim_scores, zero_cutoff = zero_cutoff, n_anchors_pos = n_anchors_pos, dim_id = dim_id, path_ptiles = path_ptiles):
+def get_ptiles(dim_scores, zero_cutoff, n_anchors_pos, dim_id, path_ptiles):
     # select imgs above cutoff
     img_ind_nonzero = list(np.where(dim_scores > zero_cutoff)[0])
     # select one training image for each anchor (first image of anchor)
@@ -50,7 +50,7 @@ def get_ptiles(dim_scores = dim_scores, zero_cutoff = zero_cutoff, n_anchors_pos
     np.savetxt(fname, ptiles_all, delimiter=",", comments='')  # save as .csv
     return ptiles_all, ptiles_nonzero, img_ind_nonzero, img_ind_zero
 
-def get_anchor_imgs(n_anchor_imgs = n_anchor_imgs, ptiles_nonzero = ptiles_nonzero, img_ind_nonzero = img_ind_nonzero, img_ind_zero = img_ind_zero, n_anchors_pos = n_anchors_pos, stim_imgs_20 = stim_imgs_20, path_exp_screenshot = path_exp_screenshot):
+def get_anchor_imgs(n_anchor_imgs, ptiles_nonzero, img_ind_nonzero, img_ind_zero, n_anchors_pos, stim_imgs_20, path_exp_screenshot):
     # initialize anchor img code dict
     anchor_img_codes = {}
     anchor_images_examples = []
@@ -82,7 +82,7 @@ def get_anchor_imgs(n_anchor_imgs = n_anchor_imgs, ptiles_nonzero = ptiles_nonze
         pickle.dump(anchor_img_codes, f)
     return anchor_images_examples, n_anchor_imgs_very, anchor_dev
 
-def create_trial_mat(fb: int, n_trials_train_mat, train_img_codes, ptiles_all = ptiles_all, dim_scores = dim_scores):
+def create_trial_mat(fb: int, n_trials_train_mat, train_img_codes, ptiles_all, dim_scores):
     trial_mat = np.zeros((n_trials_train_mat, 4))
     trial_mat[:, 0] = train_img_codes
     train_img_codes_ind = [int(x) for x in train_img_codes]  # convert img codes back to indices
@@ -91,14 +91,16 @@ def create_trial_mat(fb: int, n_trials_train_mat, train_img_codes, ptiles_all = 
     trial_mat[:, 3] = [ptiles_all[i] for i in train_img_codes_ind]  # add true_dim_score_ptile
     return trial_mat
 
-def save_train_trials(object_indices = object_indices, anchor_images_examples = anchor_images_examples, stim_imgs_20 = stim_imgs_20, n_trials_train = n_trials_train, n_trials_train_nofb = n_trials_train_nofb, n_anchors_pos = n_anchors_pos, n_blocks_train = n_blocks_train, header = header, path_output = path_output):
+def save_train_trials(object_indices, anchor_images_examples, stim_imgs_20, n_trials_train, n_trials_train_nofb,
+                      n_anchors_pos, n_blocks_train, header, path_output):
     # select training images set to sample from, without anchor images and 20 test images
     train_img_pop = [x for x in object_indices if (x not in anchor_images_examples) and (x not in stim_imgs_20)]
     # randomly select n_training_imgs-n_anchors images
     train_img_codes_sample = random.sample(train_img_pop, n_trials_train - n_anchors_pos)
     # create no-feedback training trials and save as csv
     train_img_codes_nofb = random.sample(train_img_codes_sample, n_trials_train_nofb)
-    trial_mat_nofb = create_trial_mat(fb = 0, n_trials_train_mat = n_trials_train_nofb, train_img_codes = train_img_codes_nofb)
+    trial_mat_nofb = create_trial_mat(fb = 0, n_trials_train_mat = n_trials_train_nofb,
+        train_img_codes = train_img_codes_nofb, ptiles_all = ptiles_all, dim_scores = dim_scores)
     fname = path_output + 'condition files/traintrials_nofb.csv'  # set file name
     np.savetxt(fname, trial_mat_nofb, delimiter=",", header=header, comments='')   # save as .csv
     # add anchor training images to list, and shuffle
@@ -106,7 +108,8 @@ def save_train_trials(object_indices = object_indices, anchor_images_examples = 
     train_img_codes_fb = train_img_codes_sample2 + anchor_images_examples
     random.shuffle(train_img_codes_fb)
     # create trial matrix and fill with img_codes, corresponding dim_scores, and feedback=1
-    trial_mat_fb = create_trial_mat(fb = 1, n_trials_train_mat = n_trials_train - n_trials_train_nofb, train_img_codes = train_img_codes_fb)
+    trial_mat_fb = create_trial_mat(fb = 1, n_trials_train_mat = n_trials_train - n_trials_train_nofb,
+        train_img_codes = train_img_codes_fb, ptiles_all = ptiles_all, dim_scores = dim_scores)
         # split feedback training trials in blocks and save trial files
     trial_mat_list = np.split(trial_mat_fb, n_blocks_train)
     for block, trial_mat_split in enumerate(trial_mat_list):
@@ -117,7 +120,8 @@ def save_train_trials(object_indices = object_indices, anchor_images_examples = 
     stim_imgs_train = train_img_codes_fb + train_img_codes_nofb
     return stim_imgs_train
 
-def save_exp_trials(dim_scores = dim_scores, n_blocks_exp = n_blocks_exp, n_trials_per_block = n_trials_per_block, n_trials_fb_per_block = n_trials_fb_per_block, n_trials_nofb_per_block = n_trials_nofb_per_block, trials_fb = trials_fb, trials_nofb = trials_nofb, ptiles_all = ptiles_all, stim_imgs_20 = stim_imgs_20, header = header, path_output = path_output):
+def save_exp_trials(dim_scores, n_blocks_exp, n_trials_per_block, n_trials_fb_per_block, n_trials_nofb_per_block,
+                    trials_fb, trials_nofb, ptiles_all, stim_imgs_20, header, path_output):
     range_scores_nonzero = max(dim_scores) - min(dim_scores[np.where(dim_scores > 0.3)])
     # create exp trial matrix blockwise
     for block in range(n_blocks_exp):
@@ -151,7 +155,7 @@ path_exps_final = 'C:/Private/Studium/Studium Leipzig/Masterarbeit/DimRating/fin
 path_ptiles = 'C:/Users/joper/PycharmProjects/dimRating/output/percentiles/'
 
 # load/create data
-spose, stim_imgs_20, stim_imgs_48_nonref, stim_imgs_48_new = get_data()
+spose, stim_imgs_20, stim_imgs_48_nonref, stim_imgs_48_new = get_data(path_input = path_input)
 
 # set params
 n_trials_train = 190  # number of total training trials
@@ -180,17 +184,24 @@ n_trials_nofb_per_block = int(len(trials_nofb) / n_blocks_exp)
 
 # loop over dims
 for dim_id in range(np.size(spose, 1)):
-    path_output = get_dim_paths()  # get output path
+    path_output = get_dim_paths(dim_id = dim_id, path_exps_final = path_exps_final)  # get output path
     dim_scores = spose[:, dim_id]  # select dimension specific data
     object_indices = list(np.arange(len(dim_scores)))  # create list of indices to convert to img codes
     # compute percentiles
-    ptiles_all, ptiles_nonzero, img_ind_nonzero, img_ind_zero = get_ptiles()
+    ptiles_all, ptiles_nonzero, img_ind_nonzero, img_ind_zero = get_ptiles(dim_scores = dim_scores,
+        zero_cutoff = zero_cutoff, n_anchors_pos = n_anchors_pos, dim_id = dim_id, path_ptiles = path_ptiles)
     # generate anchor img links
-    anchor_images_examples, n_anchor_imgs_very, anchor_dev = get_anchor_imgs()
+    anchor_images_examples, n_anchor_imgs_very, anchor_dev = get_anchor_imgs(n_anchor_imgs=n_anchor_imgs,
+        ptiles_nonzero = ptiles_nonzero, img_ind_nonzero = img_ind_nonzero, img_ind_zero = img_ind_zero,
+            n_anchors_pos = n_anchors_pos, stim_imgs_20 = stim_imgs_20, path_exp_screenshot = path_exp_screenshot)
     # generate and save training trial csv's
-    stim_imgs_train = save_train_trials()
+    stim_imgs_train = save_train_trials(object_indices = object_indices, anchor_images_examples=anchor_images_examples,
+        stim_imgs_20 = stim_imgs_20, n_trials_train = n_trials_train, n_trials_train_nofb = n_trials_train_nofb,
+            n_anchors_pos = n_anchors_pos, n_blocks_train = n_blocks_train, header = header, path_output = path_output)
     # generate exp trial csv
-    save_exp_trials()
+    save_exp_trials(dim_scores = dim_scores, n_blocks_exp = n_blocks_exp, n_trials_per_block = n_trials_per_block,
+        n_trials_fb_per_block = n_trials_fb_per_block, n_trials_nofb_per_block = n_trials_nofb_per_block, trials_fb = trials_fb,
+            trials_nofb = trials_nofb, ptiles_all = ptiles_all, stim_imgs_20 = stim_imgs_20, header = header, path_output = path_output)
 
     # get all possible imgs for last (=highest) anchor
     # select after creating training trials, to avoid excluding all high imgs from training
