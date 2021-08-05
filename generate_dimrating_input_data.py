ccb72e35c34aa5316a28df17b3984763853f13f9
jonasperkuhn <joperkuhn@gmx.de>
2021-08-05 13:01:34 +0200

    creating functions

--- generate_dimrating_input_data.py
+++ generate_dimrating_input_data.py
@@ -12,12 +12,15 @@ path_exps_final = 'C:/Private/Studium/Studium Leipzig/Masterarbeit/DimRating/fin
 # set path to dimRating output folder, where percentiles will be saved
 path_ptiles = 'C:/Users/joper/PycharmProjects/dimRating/output/percentiles/'
 
+# load/create data
+spose, stim_imgs_20, stim_imgs_48_nonref, stim_imgs_48_new = get_data()
+
 # set params
 n_trials_train = 190  # number of total training trials
 n_trials_train_nofb = 10  # number of trials were no feedback will be given anymore
 n_anchors = 7  # number of dim scale anchors
 n_anchors_pos = n_anchors - 1
-anchor_step_scaled = 1/(n_anchors-1)
+anchor_step_scaled = 1/(n_anchors_pos)
 n_anchor_imgs = 15  # max number of imgs per anchor
 n_anchor_imgs_insp_highest = 36  # max number of imgs displayed in the inspect_highest_anchor_imgs routine
 zero_cutoff = 0.3  # scale cutoff, to calculate percentiles separately below and above cutoff for a percentile scale that also differentiates the high values (else biased by many low values)
@@ -25,9 +28,6 @@ n_blocks_train = 3  # number of training blocks
 n_blocks_exp = 2  # number of experimental blocks: set to a number that 96, 20, and (96 + 20 =) 116 can be divided by -> 2 or 4
 header = 'img_code,dim_score_true,feedback,dim_score_true_ptile'  # variable names in input csv's
 random.seed(808)
-
-# load/create data
-spose, stim_imgs_20, stim_imgs_48_nonref, stim_imgs_48_new = get_data()
 
 # combine 48 and 48 to all_no-feedback_trials and randomize
 trials_fb = stim_imgs_20  # don't random shuffle to keep alignment to indices
@@ -42,74 +42,15 @@ n_trials_nofb_per_block = int(len(trials_nofb) / n_blocks_exp)
 
 # loop over dims
 for dim_id in range(np.size(spose, 1)):
-    ### create paths and compute variables
-    # copy template experiment folder n_dims times, to create separate experiments for each spose dimension
-        # first create template experiment folder manually, with resources subfolders for condition files, screenshots,
-        # and test images, as well as the mouse-cursor-img.png
-    dim_exp_folder_path = path_exps_final + 'dimrating_dim' + str(dim_id) + '/'
-    copytree(path_exps_final + 'dimrating_template_folder', dim_exp_folder_path)
-    # define output path as resources folder of newly created experiment folder
-    path_output = dim_exp_folder_path + 'resources/'
-    # select data relevant for dimension and add img codes
-    dim_scores = spose[:, dim_id]
+    path_output = get_dim_paths()  # get output path
+    dim_scores = spose[:, dim_id]  # select dimension specific data
     object_indices = list(np.arange(len(dim_scores)))  # create list of indices to convert to img codes
 
-    ### compute percentiles
-    # select imgs above cutoff
-    img_ind_nonzero = list(np.where(dim_scores > zero_cutoff)[0])
-    # select one training image for each anchor (first image of anchor)
-    anchor_images_examples = []
-    # get anchor imgs
-    dim_scores_nonzero = [dim_scores[ind] for ind in img_ind_nonzero]  # get dim scores corresponding to nonzero imgs
-    n_anchor_imgs_very = int(len(img_ind_nonzero) / n_anchors_pos / 2)  # get maximum nr of images for highest anchor
-    # compute percentiles for nonzero imgs for feedback
-    ptiles_nonzero = [(len(list(np.where(np.array(dim_scores_nonzero) <= score)[0])) /
-                       len(dim_scores_nonzero)) * 100 for score in dim_scores_nonzero]  # convert scores to percentiles
-    ptiles_nonzero_scaled = [((n_anchors_pos-1)*anchor_step_scaled * ptile + 100*anchor_step_scaled) for
-                             ptile in ptiles_nonzero]  # rescale to anchor_step_scaled-100 scale for non 'not at all' items
-    # compute percentiles for zero imgs for feedback
-    img_ind_zero = list(np.where(dim_scores <= zero_cutoff)[0])  # select imgs below cutoff
-    dim_scores_zero = [dim_scores[ind] for ind in img_ind_zero]  # get dim scores corresponding to notatall imgs
-    ptiles_zero = [(len(list(np.where(np.array(dim_scores_zero) <= score)[0])) /
-                    len(dim_scores_zero)) * 100 for score in dim_scores_zero]  # convert scores to percentiles
-    ptiles_zero_scaled = [anchor_step_scaled * ptile for ptile in ptiles_zero]  # rescale to 0-anchor_step_scaled scale for 'not at all' items
-    # save ptiles in same order as all dim scores
-    ptiles_all = list(range(len(dim_scores)))
-    for nonzero_ind, all_ind in enumerate(img_ind_nonzero):
-        ptiles_all[all_ind] = ptiles_nonzero_scaled[nonzero_ind]
-    for zero_ind, all_ind in enumerate(img_ind_zero):
-        ptiles_all[all_ind] = ptiles_zero_scaled[zero_ind]
-    fname = path_ptiles + 'ptiles_dim' + dim_id + '.csv'  # set file name
-    np.savetxt(fname, ptiles_all, delimiter=",", comments='')   # save as .csv
+    # compute percentiles
+    ptiles_all, ptiles_nonzero, img_ind_nonzero, img_ind_zero = get_ptiles()
 
-
-    ### generate anchor img links
-    # initialize anchor img code dict
-    anchor_img_codes = {}
-    # randomly choose n_anchor_imgs imgs of zero imgs (below cutoff) as anchor imgs
-    anchor_img_codes[0] = np.random.choice(img_ind_zero, n_anchor_imgs, replace=False)
-    # extract image codes for each anchor range, and sort from closest to furthest away
-    for i_anchor in range(n_anchors_pos):
-        # determine anchor percentile
-        ptile_anchor = i_anchor / (n_anchors_pos - 1) * 100
-        # calculate percentile deviance of each percentile
-        anchor_dev = [np.abs(ptile - ptile_anchor) for ptile in ptiles_nonzero]
-        # select n_anchor_imgs of lowest deviating percentiles
-        # for very high and very low anchor, choose max. n_anchor_imgs_very imgs, to avoid img overlap to other anchors
-        if i_anchor in [0, n_anchors_pos - 1]:
-            img_codes_closest = [img_ind_nonzero[img]
-                                 for img in np.argsort(anchor_dev)][0:min(n_anchor_imgs, n_anchor_imgs_very)]
-        # for other anchors, take all n_anchor_imgs imgs (no threat of overlap)
-        else:
-            img_codes_closest = [img_ind_nonzero[img] for img in np.argsort(anchor_dev)][0: n_anchor_imgs]
-        # remove previously rated 20 images (because they will be tested)
-        anchor_img_codes[i_anchor + 1] = [int(img_code) for img_code in img_codes_closest
-                                          if img_code not in stim_imgs_20]
-
-        anchor_images_examples.append(anchor_img_codes[i_anchor + 1][-1])  # append last (furthest away) anchor image
-    # save codes of each anchor as pickle
-    with open(path_exp_screenshot + 'anchor_img_codes/anchor_img_codes_' + str(dim_id) + '.pkl', 'wb') as f:
-        pickle.dump(anchor_img_codes, f)
+    # generate anchor img links
+    anchor_images_examples, n_anchor_imgs_very, anchor_dev = get_anchor_imgs()
 
     ### generate training trial csv
     # select training images set to sample from, without anchor images and 20 test images
