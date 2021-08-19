4a0a2fbab4425925fefd64c209de47eb56b60004
jonasperkuhn <joperkuhn@gmx.de>
2021-08-19 12:11:18 +0200

    changed folder names

--- rename_files.py
+++ rename_files.py
@@ -1,22 +1,21 @@
 # rename files in folder
 import os
 import csv
-
 path = 'C:/Users/joper/PycharmProjects/dimRating/data/'
+
+# rename files with unique_id index
 # load unique_ids (img names in sequence as spose embedding)
 with open(path + 'unique_id.csv', newline='') as f:
     reader = csv.reader(f)
     unique_ids = [i[0] + '.jpg' for i in list(reader)]
-path_imgs = path + 'images_old names'
+path_imgs = path + 'images_old_names'
 files = os.listdir(path_imgs)
-# rename files with unique_id index
 for file in files:
     index = unique_ids.index(file)
     os.rename(os.path.join(path_imgs, file), os.path.join(path_imgs, ''.join([str(index), '.jpg'])))
 
-# rename new images files
-import os
-path = 'C:/Private/Studium/Studium Leipzig/Masterarbeit/DimRating/Stimuli/48new_200'
-files = os.listdir(path)
+# rename new images files with arbitrary code
+path_imgs = path + 'images_old_names'
+files = os.listdir(path_imgs)
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index+3000), '.jpg'])))
+    os.rename(os.path.join(path_imgs, file), os.path.join(path_imgs, ''.join(['{:02d}'.format(index+1), '.jpg'])))
