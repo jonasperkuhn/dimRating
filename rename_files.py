885ce82c3593921e7cf57f8bd8dfde15c125f364
jonasperkuhn <joperkuhn@gmx.de>
2021-08-23 16:07:22 +0200

    added code to get convert unique id names to indices

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,7 @@
 # rename files in folder
 import os
 import csv
+import numpy as np
 path = 'C:/Users/joper/PycharmProjects/dimRating/data/'
 
 # rename files with unique_id index
@@ -19,3 +20,13 @@ path_imgs = path + 'images_old_names'
 files = os.listdir(path_imgs)
 for index, file in enumerate(files):
     os.rename(os.path.join(path_imgs, file), os.path.join(path_imgs, ''.join(['{:02d}'.format(index+1), '.jpg'])))
+
+# get indices of ref test images from unique ids
+# load list of reference test stimuli
+with open(path + 'test_imgs_200_names.txt', newline='') as f:
+    reader = csv.reader(f)
+    test_imgs_200_named = [i[0] + '.jpg' for i in list(reader)]
+# convert list of ref test stimuli from names to unique_id_indices
+ref_imgs_200 = [unique_ids.index(img) for img in test_imgs_200_named]
+fname = "ref_imgs_200.txt"
+np.savetxt(fname, ref_imgs_200, delimiter=",", comments='')
